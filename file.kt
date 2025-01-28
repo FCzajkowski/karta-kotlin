8aaeed8eed99829201e8473a9588c50eb0a922f3
Franciszek <FCzajkowski@proton.me>
2025-01-28 13:18:44 +0100

    Create file.kt

---
+++ file.kt
@@ -0,0 +1,208 @@
+/* 
+1)
+fun main(){} / c
+
+2).kt
+
+3) nie
+
+4) podobne ale inne
+
+5) println()
+
+6) new Scanner(System.in)
+
+7) val zmienna: Int = 0;
+
+8) Int, Float/Double, String, Boolean, Char
+
+9) zmienno-przecinkowego (Double)
+
+10) var zmienna: String? = null
+
+11) val zmienna: String? = null
+
+12) var zmienna: Int? = null
+
+13) temperatura?.toInt()
+
+14) błąd
+
+15) przedzial liczb
+
+16) and 
+
+17) or 
+
+18) a += 5
+
+19) CzyZnajomiMajaCosDoPicia || CzySklepJestBlisko
+
+20) wszystkie są poprawne
+
+21) wszystkie są poprawne poza pierwszą odpowiedzią 
+
+22) wszystkie są poprawne
+
+23) nic nie ujrzymy
+
+24) ADFG
+
+25) wszystkie prawidłowe
+
+26) c jest nieprawidłowe
+
+27) d jest nieprawidłowe
+
+28) 963
+
+29) 12345678
+
+30) 0123456
+
+31 LUSTRA
+
+32) wszystkie są prawidłowe
+
+33) zmienna.length()
+
+34) 
+
+
+35) 7+10
+
+36) tekst.uppercase()
+
+37
+val jeden = "heja"
+val dwa = "heja"
+
+if(jeden == dwa)
+{
+  println("ciągi takie same")
+}
+else
+{
+  println("ciągi są inne")
+}
+
+ 38
+val str1 = "Hello"
+val str2 = "Hello"
+
+println(
+str1 == str2
+)
+
+ 39
+
+fun dodawanie(a: Int, b:Int): Int {
+  return a + b;
+}
+
+40: null
+
+41) wszystkie są prawidłowe
+
+42) dodaj(z = 1, y = 2, x = 3)
+
+43 
+fun dodaj(a: Int, b: Int): Int = a + b
+
+44) 13 
+
+45) 1 
+
+46) 0
+
+47) 
+val artykul = Artykul("Tytul", "Tekst")
+
+48) val artykul = Artykul()
+
+49)
+
+50)
+internal
+public 
+protected 
+private
+
+
+51) this.
+
+52) metodą
+
+53) open
+
+54)
+55)
+56)
+57) Any()
+58) lateinit
+59) wszystkie odp są prawidłowe
+
+60) wszystkie są prawidłowe
+
+61) is<Type>
+np:
+isString()
+
+62)
+
+63) wszystkie prawidłowe
+
+64) zmienna jest juź utworzona w pamięci w czasie kompilacji programu
+
+65)
+fun Int.czyParzysta(): Boolean{
+  return Int % 2 == 0
+}
+
+66) dodaje zachowanie już do istniejącej klasy
+
+67) Tekst z Extension function
+
+68) operator
+
+69) możemy przeciążać operatory zdefiniowane w dokumentacji
+
+70) 2 potega 3 
+
+71) infix fun Double.potega(potega:Double): Double = this.pow(potega)
+
+72)wszystkie są prawidłowe
+
+73) abstract class klasa
+
+74) class syn: Ojciec()
+
+75) interface interfejs{}
+
+76)
+class klasa{
+  interface interfejs{
+  
+  }
+}
+
+77) konstruktor musi mieć max 1 
+
+78)
+procesor=3.2, pamiec=8, dysk=256
+
+79)
+służy do tworzenia obiektów 
+
+80) sealed class
+*/
+
+// ============== ZADANIA PRAKTYCZNE ==============
+
+
+fun zad1(){
+  println("Witaj Świecie")
+}
+fun main(){
+  zad1()
+}
